da53787bb423f87011c132ca7435a6d562257221
Timothy Borghouts <Timothy.borghouts2@gmail.com>
2022-07-02 13:21:58 +0200

    added validateToken method before every meal route

--- src/routes/meal.routes.js
+++ src/routes/meal.routes.js
@@ -3,18 +3,34 @@ const router = express.Router();
 const mealController = require("../controllers/meal.controller");
 
 //UC-301 - Toevoegen van een maaltijd.
-router.post("/api/meal", mealController.addMeal);
+router.post("/api/meal", authController.validateToken, mealController.addMeal);
 
 //UC-302 - Bekijken van alle maaltijden.
-router.get("/api/meal", mealController.getAllMeals);
+router.get(
+  "/api/meal",
+  authController.validateToken,
+  mealController.getAllMeals
+);
 
 //UC-303 - Een specifieke maaltijd opvragen.
-router.get("/api/meal/:mealId", mealController.getMealById);
+router.get(
+  "/api/meal/:mealId",
+  authController.validateToken,
+  mealController.getMealById
+);
 
 //UC-304 - Verander een specifieke maaltijd.
-router.put("/api/meal/:mealId", mealController.updateMealById);
+router.put(
+  "/api/meal/:mealId",
+  authController.validateToken,
+  mealController.updateMealById
+);
 
 //UC-305 - Verwijder een specifieke maaltijd
-router.delete("/api/meal/:mealId", mealController.deleteMealById);
+router.delete(
+  "/api/meal/:mealId",
+  authController.validateToken,
+  mealController.deleteMealById
+);
 
 module.exports = router;
