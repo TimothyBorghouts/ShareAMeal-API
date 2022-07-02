d21f0c23df78f8ddb0c34f0a4738495966eccf08
Timothy Borghouts <Timothy.borghouts2@gmail.com>
2022-07-02 13:27:58 +0200

    Added authController in the meal.routes.js

--- src/routes/meal.routes.js
+++ src/routes/meal.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("../controllers/auth.controller");
 const mealController = require("../controllers/meal.controller");
 
 //UC-301 - Toevoegen van een maaltijd.
