2eb4f81058641e59593271ef2a547bb847ec3503
Timothy Borghouts <timothy.borghouts2@gmail.com>
2023-07-04 17:57:17 +0200

    Ristretto

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
-//UC-101 - Inloggen
-router.post('/api/login', authController.validateLogin, authController.login);
+// //UC-101 - Inloggen
+// router.post('/api/login', authController.validateLogin, authController.login);
 
 //UC-102 - Opvragen van systeeminformatie
 router.get('/api/info', authController.info);
