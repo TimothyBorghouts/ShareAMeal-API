113e7074001bfb94484760601e0bda020dc44f4e
Timothy Borghouts <timothy.borghouts2@gmail.com>
2023-07-04 17:05:51 +0200

    Removed funtionality to test if ristretto server works

--- src/routes/meal.routes.js
+++ src/routes/meal.routes.js
@@ -3,26 +3,26 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 const mealController = require('../controllers/meal.controller');
 
-//UC-301 - Toevoegen van maaltijd
-router.post('/api/meal', authController.validateToken, mealController.validateMealInput, mealController.addMeal);
+// //UC-301 - Toevoegen van maaltijd
+// router.post('/api/meal', authController.validateToken, mealController.validateMealInput, mealController.addMeal);
 
-//UC-302 - Wijzigen van maaltijdgegevens
-router.put(
-  '/api/meal/:mealId',
-  authController.validateToken,
-  mealController.validateMealExistence,
-  mealController.validateMealInput,
-  mealController.validateMealOwnership,
-  mealController.updateMealById
-);
+// //UC-302 - Wijzigen van maaltijdgegevens
+// router.put(
+//   '/api/meal/:mealId',
+//   authController.validateToken,
+//   mealController.validateMealExistence,
+//   mealController.validateMealInput,
+//   mealController.validateMealOwnership,
+//   mealController.updateMealById
+// );
 
-//UC-303 - Opvragen van alle maaltijden
-router.get('/api/meal', authController.validateToken, mealController.getAllMeals);
+// //UC-303 - Opvragen van alle maaltijden
+// router.get('/api/meal', authController.validateToken, mealController.getAllMeals);
 
-//UC-304 - Opvragen van maaltijd bij ID
-router.get('/api/meal/:mealId', authController.validateToken, mealController.validateMealExistence, mealController.getMealById);
+// //UC-304 - Opvragen van maaltijd bij ID
+// router.get('/api/meal/:mealId', authController.validateToken, mealController.validateMealExistence, mealController.getMealById);
 
-//UC-305 - Verwijderen van maaltijd
-router.delete('/api/meal/:mealId', authController.validateToken, mealController.validateMealExistence, mealController.validateMealOwnership, mealController.deleteMealById);
+// //UC-305 - Verwijderen van maaltijd
+// router.delete('/api/meal/:mealId', authController.validateToken, mealController.validateMealExistence, mealController.validateMealOwnership, mealController.deleteMealById);
 
 module.exports = router;
--- src/routes/participate.routes.js
+++ src/routes/participate.routes.js
@@ -3,36 +3,36 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 const participateController = require('../controllers/participate.controller');
 
-//UC-401 Aanmelden voor maaltijd
-router.post('/api/meal/:mealId/participate', authController.validateToken, participateController.validateMealExistence, participateController.participateToMeal);
+// //UC-401 Aanmelden voor maaltijd
+// router.post('/api/meal/:mealId/participate', authController.validateToken, participateController.validateMealExistence, participateController.participateToMeal);
 
-//UC-402 Afmelden voor maaltijd
-router.delete(
-  '/api/meal/:mealId/participate',
-  authController.validateToken,
-  participateController.validateMealExistence,
-  participateController.checkIfParticipateExists,
-  participateController.participateLeaveMeal
-);
+// //UC-402 Afmelden voor maaltijd
+// router.delete(
+//   '/api/meal/:mealId/participate',
+//   authController.validateToken,
+//   participateController.validateMealExistence,
+//   participateController.checkIfParticipateExists,
+//   participateController.participateLeaveMeal
+// );
 
-//UC-403 Opvragen van deelnemers
-router.get(
-  '/api/meal/:mealId/participants',
-  authController.validateToken,
-  participateController.validateMealExistence,
-  participateController.validateMealOwnership,
-  participateController.checkIfParticipateExists,
-  participateController.getAllParticipants
-);
+// //UC-403 Opvragen van deelnemers
+// router.get(
+//   '/api/meal/:mealId/participants',
+//   authController.validateToken,
+//   participateController.validateMealExistence,
+//   participateController.validateMealOwnership,
+//   participateController.checkIfParticipateExists,
+//   participateController.getAllParticipants
+// );
 
-//UC-404 Opvragen van details van deelnemer
-router.get(
-  '/api/meal/:mealId/participants/:userId',
-  authController.validateToken,
-  participateController.validateMealExistence,
-  participateController.validateMealOwnership,
-  participateController.checkIfParticipateExists,
-  participateController.getParticipantsDetails
-);
+// //UC-404 Opvragen van details van deelnemer
+// router.get(
+//   '/api/meal/:mealId/participants/:userId',
+//   authController.validateToken,
+//   participateController.validateMealExistence,
+//   participateController.validateMealOwnership,
+//   participateController.checkIfParticipateExists,
+//   participateController.getParticipantsDetails
+// );
 
 module.exports = router;
