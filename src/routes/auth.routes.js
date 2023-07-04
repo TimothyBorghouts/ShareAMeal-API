97c74fe57748b99e046d8bf4e26f072d4d93d2dc
Timothy Borghouts <timothy.borghouts2@gmail.com>
2023-07-04 17:09:08 +0200

    Uncommented more functionality to see if it works on Ristretto

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
-// //UC-101 - Inloggen
-// router.post('/api/login', authController.validateLogin, authController.login);
+//UC-101 - Inloggen
+router.post('/api/login', authController.validateLogin, authController.login);
 
-// //UC-102 - Opvragen van systeeminformatie
-// router.get('/api/info', authController.info);
+//UC-102 - Opvragen van systeeminformatie
+router.get('/api/info', authController.info);
 
 module.exports = router;
